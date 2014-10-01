d87d471953a6a944e3ccca686bbf14f50782fe53
billpsyllos <billpsyllos1@gmail.com>
2014-10-01 20:01:53 +0300

    Created a new activity 'EditFriendsActivity' and build query to retrieve data from backend and display them to a list

---
+++ app/src/main/java/com/example/messageapp/messageapp/ParseConstants.java
@@ -0,0 +1,12 @@
+package com.example.messageapp.messageapp;
+
+/**
+ * Created by billaros on 1/10/2014.
+ */
+public final class ParseConstants {
+    //Class name
+
+
+    //Fields names
+    public static final String KEY_USERNAME = "username";
+}
