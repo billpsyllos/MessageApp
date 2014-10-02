a498ec8283de644d5f7216837391aa47f44c5f32
billpsyllos <billpsyllos1@gmail.com>
2014-10-02 03:09:35 +0300

    Created a new activity 'Edit Friends' to store a list and select friends. Save friends RelationShips using Parse API and added checkmarks for already friends...Also display our friends to Friends Fragment..and finaly created option to remove  friends

--- app/src/main/java/com/example/messageapp/messageapp/EditFriendsActivity.java
+++ app/src/main/java/com/example/messageapp/messageapp/EditFriendsActivity.java
@@ -7,13 +7,17 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
+import com.parse.ParseRelation;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.util.List;
 
@@ -23,17 +27,24 @@ public class EditFriendsActivity extends ListActivity {
     public  static  final String TAG = EditFriendsActivity.class.getSimpleName();
 
     protected List<ParseUser> mUsers ;
+    protected ParseRelation <ParseUser> mFriendsRelation;
+    protected ParseUser mCurrentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.activity_edit_friends);
+
+        getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+
+        mCurrentUser = ParseUser.getCurrentUser();
+        mFriendsRelation = mCurrentUser.getRelation(ParseConstants.KEY_FRIENDS_RELATION);
 
         setProgressBarIndeterminateVisibility(true);
 
@@ -57,6 +68,8 @@ public class EditFriendsActivity extends ListActivity {
                             android.R.layout.simple_list_item_checked,usernames);
                     setListAdapter(adapter);
 
+                    addFriendsCheckMarks();
+
                 }else{
                     Log.e(TAG, e.getMessage());
                     AlertDialog.Builder builder = new AlertDialog.Builder(EditFriendsActivity.this);
@@ -72,13 +85,6 @@ public class EditFriendsActivity extends ListActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.edit_friends, menu);
-        return true;
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
@@ -88,5 +94,48 @@ public class EditFriendsActivity extends ListActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, final int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        if (getListView().isItemChecked(position)) {
+            //add friends
+            mFriendsRelation.add(mUsers.get(position));
+        }else{
+            //remove friends
+            mFriendsRelation.remove(mUsers.get(position));
+        }
+        mCurrentUser.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, e.getMessage());
+                }
+            }
+        });
+    }
+
+    private void  addFriendsCheckMarks(){
+        mFriendsRelation.getQuery().findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> friends, ParseException e) {
+                if(e== null){
+                    //list returned - look for a match
+                    for (int i = 0; i<mUsers.size(); i++){
+                       ParseUser user =  mUsers.get(i);
+                       for(ParseUser friend :friends){
+                           //use equals to compare string in java
+                           if (friend.getObjectId().equals(user.getObjectId())){
+                                getListView().setItemChecked(i, true);
+                           }
+                       }
+                    }
+                }else{
+
+                    Log.e(TAG,e.getMessage());
+                }
+            }
+        });
     }
 }
